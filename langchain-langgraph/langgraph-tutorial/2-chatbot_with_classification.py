c022e9955214d81fc3d5ce4ea6306b53bb1c6c69
Borhan Darvishi <borhandr23@gmail.com>
2025-06-09 22:57:05 +0330

    create classifier chatbot with route node

--- langchain-langgraph/langgraph-tutorial/2-chatbot_with_classification.py
+++ langchain-langgraph/langgraph-tutorial/2-chatbot_with_classification.py
@@ -13,33 +13,128 @@ from typing_extensions import TypedDict
 
 
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_API_KEY"] =  os.getenv("OPENAI_API_KEY")
 
 
 llm = init_chat_model("gpt-4o-mini", temperature=0)
 
-# the state is defineing the type of information that we want to have in our graph.
+
+
+class MessageClassifier(BaseModel):
+    message_type: Literal["emotional","logical"] = Field(
+        ...,
+        description="Classify if the message requires an emotional (therapist) or logical response." 
+
+# the state is defineing the type of information that we want to have  in our graph.
 class State(TypedDict):
     messages: Annotated[list,add_messages]
+    message_type: str | None
+
+
+
+def classify_message(state: State) -> State:
+    last_message = state["messages"][-1]
+    classifier_llm = llm.with_structured_output(MessageClassifier)
+
+    result = classifier_llm.invoke([
+        {
+            "role": "systemt",
+            "content": """Classify the user message as either:
+            - 'emotional': if it asks for emotional support, therapy, deals with feelings, or personal problems
+            - 'logical': if it asks for facts, information, logical analysis, or practical solutions
+            """
+        },
+        {
+            "role": "user",
+            "content": last_message.content
+        }
+    ])
+
+    return {"message_type": result.message_type}
+    
+def router(state: State) -> State:
+    message_type = state.get("message_type","logical")
+    if message_type == "emotional":
+        return {"next":"therapist"}
+    
+    else:
+        return {"next":"logical"}   
+    
+
+def therapist_agent(state: State) -> State:
+    last_message = state["messages"][-1]
+
+    messages = [
+        {"role": "system",
+         "content": """You are a compassionate therapist. Focus on the emotional aspects of the user's message.
+                        Show empathy, validate their feelings, and help them process their emotions.
+                        Ask thoughtful questions to help them explore their feelings more deeply.
+                        Avoid giving logical solutions unless explicitly asked."""
+         },
+        {
+            "role": "user",
+            "content": last_message.content
+        }
+    ]
+
+    reply = llm.invoke(messages)
+     return {"messages": [{"role": "assistant", "content": reply.content}]}
+    
+
+def logical_agent(state: State):
+    last_message = state["messages"][-1]
+
+    messages = [
+        {"role": "system",
+         "content": """You are a purely logical assistant. Focus only on facts and information.
+            Provide clear, concise answers based on logic and evidence.
+            Do not address emotions or provide emotional support.
+            Be direct and straightforward in your responses."""
+         },
+        {
+            "role": "user",
+            "content": last_message.content
+        }
+    ]
+    reply = llm.invoke(messages)
+    return {"messages": [{"role": "assistant", "content": reply.content}]}
+    
+
 
 # create a graph using state.
 # a node is a function that modify or check the state.
 graph_builder = StateGraph(State)
 
-# this is node 
-def chatbot(state: State):
-    return {"messages": [llm.invoke(state["messages"])]}
+graph_builder.add_node("classifier",classify_message)
+graph_builder.add_node("router", router)
+graph_builder.add_node("therapist", therapist_agent)
+graph_builder.add_node("logical", logical_agent)
 
-graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "classifier")
+graph_builder.add_edge("classifier", "router")
+graph_builder.add_conditional_edge(
+    "router",
+    lambda state: state.get("next"),
+    {
+        "therapist": "therapist",
+        "logical": "logical"
+    }
+)                           
 
-# except the chatbot node, we also have two special nodes: START and END (we added them automatically but the not connected by edges. lets connect them)
-graph_builder.add_edge(START, "chatbot")
-graph_builder.add_edge("chatbot", END)
+graph_builder.add_edge("therapist", END)
+graph_builder.add_edge("logical", END)  
 
-# let's run the graph
+# let's run the graph 
 graph = graph_builder.compile()
 
-user_input = input("User: ")
+def run_chatbot(user_message: str):
+    state = {"message":[],"message_type": None}
 
-state = graph.invoke({"messages": [{"role": "user", "content": user_input}]})
-print(state["messages"][-1].content)
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() in ["exit", "quit"]:
+            print("Exiting the chatbot.")
+            break
+        state["messages"] = state.get("messages",[]) + [{"role": "user", "content": user_input}]
+        
+        state = graph.invoke(state)
